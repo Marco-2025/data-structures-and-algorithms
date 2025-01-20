251ca840dddc3eabd25c00a1349592946c92f45c
marcodeanda20 <110065366+marcodeanda20@users.noreply.github.com>
2025-01-20 20:37:29 +0000

    Adding binary search example

--- linear-search.py
+++ linear-search.py
@@ -1,11 +1,11 @@
 def linear_search(array, value):
     for index, item in enumerate(array):
         if value == item:
-            print("steps:", index+1)
-            return value
-        elif value < item:
-            print("steps:", index+1)
-            break
+            return "steps: " + str(index+1)
+        elif value < item:     
+            return "steps: " + str(index+1)
+        elif index == len(array)-1:
+            return "steps: " + str(len(array))
 
 ordered_array = [1,2,3,4,5]
-print(linear_search(ordered_array, 3.3))
+print(linear_search(ordered_array, 5))
