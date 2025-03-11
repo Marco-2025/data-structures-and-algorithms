507ff30fc31ac66ab76d77e7de2f1c8080aa0972
marcodeanda20 <110065366+marcodeanda20@users.noreply.github.com>
2025-03-11 22:15:27 +0000

    added missing-letter py script

--- missing-letter-hash-table.py
+++ missing-letter-hash-table.py
@@ -1,15 +1,12 @@
 import string
 def missing_letter_finder(str):
     hash_table = {}
-    for letter in string.ascii_lowercase:
-        hash_table[letter] = False
 
     for i in str.replace(" ", ""):
-        if hash_table.get(i) != None:
-            hash_table[i] = True
+        hash_table[i] = True
 
     for a in string.ascii_lowercase:
-        if hash_table.get(a) == False:
+        if hash_table.get(a) == None:
             return a
         
 print(missing_letter_finder("the quick brown box jumps over a lazy dog"))
